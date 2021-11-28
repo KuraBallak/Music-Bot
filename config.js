8f8a5db1f684e0d30c4ebee31c2e023ff495db54
KuraBallak <81288372+KuraBallak@users.noreply.github.com>
2021-11-28 16:31:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 /* Your Bot Default Prefix For Every Server */
-exports.Default_Prefix = "";
+exports.Default_Prefix = "p!";
 /* Your Bot All Embeds Color */
 exports.Color = "";
 /* Your Bot Token (Secret) */
 exports.Token = "";
 /* Bot's Owner ID */
-exports.OwnerID = "";
+exports.OwnerID = "772027878950305802";
